ff32d452963cdec8f5ada9030257ab604202d997
Γιώργος Παγώνης <georgios.pagonis@gmail.com>
2025-07-12 15:56:07 +0000

    Τώρα θέλω όταν στην εμφάνιση του υπολογιστή όταν το side bar είναι κλεισ

--- src/components/interactive-logo.tsx
+++ src/components/interactive-logo.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { Logo } from "./logo"
-import { SidebarIcon } from "./icons"
+import { SidebarIcon, LogoIcon } from "./icons"
 import { useSidebar } from "./ui/sidebar"
 import { cn } from "@/lib/utils"
 
@@ -12,25 +12,26 @@ export function InteractiveLogo() {
 
   const isCollapsed = !open;
 
+  if (isCollapsed) {
+    return (
+       <div
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onClick={toggleSidebar}
+        className="cursor-pointer h-full flex items-center justify-center w-full p-2"
+      >
+        {hovered ? <SidebarIcon className="w-6 h-6" /> : <LogoIcon className="w-6 h-6 text-primary" />}
+      </div>
+    )
+  }
+
   return (
     <div
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
       onClick={toggleSidebar}
-      className="cursor-pointer h-full flex items-center justify-center w-full"
+      className="cursor-pointer h-full flex items-center justify-between w-full p-2"
     >
-      <div className={cn(
-        "transition-all duration-300",
-        isCollapsed ? "w-auto" : "w-full px-2"
-      )}>
-        {isCollapsed && !hovered ? (
-          <Logo />
-        ) : (
-          <div className="p-2">
-            <SidebarIcon className="w-6 h-6" />
-          </div>
-        )}
-      </div>
+      <Logo />
+      <SidebarIcon className="w-6 h-6" />
     </div>
   )
 }
